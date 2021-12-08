17136d2df2ec7351d9b85f58f1787481ecd167a6
Christopher McComas <mccomas.chris@gmail.com>
2021-12-08 15:01:23 -0500

    wip

--- endpoints.php
+++ endpoints.php
@@ -113,9 +113,8 @@ function mu_get_past_visits() {
 				array(
 					'title'                => $post->post_title,
 					'mu_visits_date'       => get_post_meta( $post->ID, 'mu_visits_date', 1 ),
-					// 'mu_visits_start_time' => get_field( 'mu_visits_start_time', $post->ID ),
 					'mu_visits_start_time' => Carbon::parse( get_field( 'mu_visits_start_time', $post->ID ) )->format( 'g:i a' ),
-					'mu_visits_end_time'   => get_field( 'mu_visits_end_time', $post->ID ),
+					'mu_visits_end_time' => Carbon::parse( get_field( 'mu_visits_end_time', $post->ID ) )->format( 'g:i a' ),
 					'mu_visits_type'       => get_post_meta( $post->ID, 'mu_visits_type', 1 ),
 				)
 			);
