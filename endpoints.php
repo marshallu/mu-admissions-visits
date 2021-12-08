7b6bbb41926e673f050be6a368b64ec1d979638a
Christopher McComas <mccomas.chris@gmail.com>
2021-12-08 15:04:40 -0500

    wip

--- endpoints.php
+++ endpoints.php
@@ -63,14 +63,23 @@ function mu_get_latest_visits() {
 
 	if ( $posts ) {
 		foreach ( $posts as $post ) {
+			$start_time = '';
+			$end_time   = '';
+
+			if ( get_field( 'mu_visits_start_time', $post->ID ) ) {
+				$start_time = Carbon::parse( get_field( 'mu_visits_start_time', $post->ID ) )->format( 'g:i a' );
+			}
+
+			if ( get_field( 'mu_visits_end_time', $post->ID ) ) {
+				$end_time = Carbon::parse( get_field( 'mu_visits_end_time', $post->ID ) )->format( 'g:i a' );
+			}
 			array_push(
 				$visits,
 				array(
 					'title'                => $post->post_title,
 					'mu_visits_date'       => get_post_meta( $post->ID, 'mu_visits_date', 1 ),
-					// 'mu_visits_start_time' => Carbon::parse( get_field( 'mu_visits_start_time', $post->ID ) )->format( 'g:i a' ),
-					'mu_visits_start_time' => get_field( 'mu_visits_start_time', $post->ID ),
-					'mu_visits_end_time'   => get_field( 'mu_visits_end_time', $post->ID ),
+					'mu_visits_start_time' => $start_time,
+					'mu_visits_end_time'   => $end_time,
 					'mu_visits_type'       => get_post_meta( $post->ID, 'mu_visits_type', 1 ),
 				)
 			);
@@ -125,7 +134,7 @@ function mu_get_past_visits() {
 					'title'                => $post->post_title,
 					'mu_visits_date'       => get_post_meta( $post->ID, 'mu_visits_date', 1 ),
 					'mu_visits_start_time' => $start_time,
-					'mu_visits_end_time'   =>  $end_time,
+					'mu_visits_end_time'   => $end_time,
 					'mu_visits_type'       => get_post_meta( $post->ID, 'mu_visits_type', 1 ),
 				)
 			);
